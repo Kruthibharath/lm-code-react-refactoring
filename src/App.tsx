8823e93fdd474e9e83be6da6cc9c2639b44b425a
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-19 13:19:28 +0000

    refactor: created individual components describe their roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { useFetch } from "./hooks/useFetch";
 import { useHero } from "./hooks/useHero";
-import { v4 as uuid } from "uuid";
 import "./App.css";
+import { Header } from "./components/header";
+import { BannerWithTestimonials } from "./components/bannerWithTestimonials";
 
 function App() {
   const [trips, setTrips] = useState(0);
@@ -33,70 +34,11 @@ function App() {
     }
   };
 
-  const renderReviewScore = (score: number) => {
-    const scoreRepresentationUsingACuteLilPlanetEmoji = [];
-    for (let i = 0; i < score; i++) {
-      scoreRepresentationUsingACuteLilPlanetEmoji.push(
-        <span key={uuid()}>🪐</span>
-      );
-    }
-    return scoreRepresentationUsingACuteLilPlanetEmoji;
-  };
-
   return (
     <>
-      <header className="site-header">
-        <div className="site-identity">
-          <a>
-            <img
-              className="eefe-logo"
-              src="/icons8-space-64.png"
-              alt="eefe spaceship logo"
-            />
-          </a>
-          <h1>Extraterrestrial Excursions for Earthlings</h1>
-        </div>
-        <ul className="go-somewhere">
-          {nav.map((place) => {
-            return (
-              <li key={place}>
-                <a>{place}</a>
-              </li>
-            );
-          })}
-        </ul>
-      </header>
-
+      <Header nav={nav} />
       {waitAMo && <p>...getting heroics...</p>}
-      {heroics && (
-        <div
-          className="hero-section"
-          style={{
-            background: `linear-gradient(0deg, rgba(10 10 10 / 90%), rgba(25 119 25 / 60%)), url(${heroics.image})`,
-            backgroundSize: "cover",
-            backgroundPosition: "50% 20%",
-          }}
-        >
-          <h3>We've been helping humanity traverse the universe for aeons.</h3>
-          <p>
-            Now it's your turn to marvel at the wonders of the universe! But don't just take our word for it; check out
-            these smashing reviews from customers just like you!
-          </p>
-          <ul className="testimonials">
-            {heroics.testimonials.map(({ name, rating, spiel, social }) => {
-              return (
-                <li key={name} className="test-card">
-                  <h4>{name}</h4>
-                  <a>{social}</a>
-                  <p>{spiel}</p>
-                  <p>Score / 5: {renderReviewScore(rating)}</p>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
-
+      {heroics && <BannerWithTestimonials pureHeroism={heroics} />}
       {isLoading && <h2>Loading...</h2>}
       {data && (
         <div className="trips">
---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+interface nav {
+  nav: string[];
+}
+
+export const Header: React.FC<nav> = ({ nav }) => {
+  return (
+    <>
+      <header className="site-header">
+        <div className="site-identity">
+          <a>
+            <img
+              className="eefe-logo"
+              src="/icons8-space-64.png"
+              alt="eefe spaceship logo"
+            />
+          </a>
+          <h1>Extraterrestrial Excursions for Earthlings</h1>
+        </div>
+        <ul className="go-somewhere">
+          {nav.map((place) => {
+            return (
+              <li key={place}>
+                <a>{place}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </header>
+      ;
+    </>
+  );
+};
